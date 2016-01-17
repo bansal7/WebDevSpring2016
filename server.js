5b8e15f95fa8dca8a546d3beb5cc8bf6b3d30945
bansal7 <shah.ba@husky.neu.edu>
2016-01-17 16:43:31 -0500

    Edited server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var app = express();
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 app.get('/hello', function(req, res){
-    res.send('hello world');
+    res.send('hello world. this is my first node.js program....');
 });
 app.listen(port, ipaddress);
