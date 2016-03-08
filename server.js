ec0dd7b9b122d8968846d6bb6e7b9f6ef968ece3
bansal7 <shah.ba@husky.neu.edu>
2016-03-07 23:24:29 -0500

    changes in server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
-var request = require('request');
-var OAuth   = require('oauth-1.0a');
+//var request = require('request');
+//var OAuth   = require('oauth-1.0a');
 var app = express();
 app.use(express.static(__dirname + '/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
