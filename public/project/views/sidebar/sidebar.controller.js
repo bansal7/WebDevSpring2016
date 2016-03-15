af2b1ec02afba20a6450eab750bb0776c4dd6582
bansal7 <shah.ba@husky.neu.edu>
2016-03-15 00:00:18 -0400

    trying the api

--- public/project/views/sidebar/sidebar.controller.js
+++ public/project/views/sidebar/sidebar.controller.js
@@ -7,23 +7,21 @@
     function SidebarController($scope,$http) {
 
         $scope.callApi=callApi;
-
-        function callApi(){
-
-            var req={
+        console.log("Hello");
+        function callApi() {
+            console.log("Hiii from inside the api call...");
+            var req = {
                 method: 'POST',
                 url: '/api',
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded'
                 },
             }
+
+            $http(req).success(render);
         }
-
-        $http(req).success(render);
-
         function render(response){
             console.log(response);
         }
     }
-
 })();
--- server.js
+++ server.js
@@ -1,34 +1,26 @@
 var express = require('express');
-var http=require('http');
-var https = require('https');
 var bodyParser = require('body-parser');
-var app = express();
-app.use(express.static(__dirname + '/public'));
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 var request = require('request');
 
-app.get('/hello', function(req, res){
-    res.send('hello world');
-});
-app.listen(port, ipaddress);
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
-
 app.post('/api', urlencodedParser, function (req, results) {
-    //console.log(b);
+
     request({
-        url: "https://secure.splitwise.com/api/v3.0/get_current_user",
+        url: "http://www.shortreckonings.com/api/1.0/query.php?e[]=50,(*1,1)&e[]=30,(2,*1)&map=Joe,Mary",
         json: true
     }, function (error, response, body) {
         if (!error && response.statusCode === 200) {
-            //console.log(response);
-            console.log(body) ;// Print the json response
-        }
-        else{
-            console.log(error);
+            results.json(body);
         }
     });
-    results.send("Thanks!");
+
 });
+app.listen(port, ipaddress);
